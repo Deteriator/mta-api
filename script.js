c9491c23f9109a87d9442cec9c1ad38912507b1b
Deteriator <markgriffith009@gmail.com>
2017-01-05 22:55:35 +0000

    Finished basic functionality

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 
 
+
 $(document).ready(function() {
-var input= $("input").val
- "https://api.giphy.com/v1/gifs/search?q=" +input+ "api_key=dc6zaTOxFJmzC"
+    $("#post_button").click(function(){
+    var input2= $("#input").val();
+ 
    $.getJSON(
      
-       "https://api.giphy.com/v1/gifs/search?q=" +input+ "api_key=dc6zaTOxFJmzC",
+       "https://api.giphy.com/v1/gifs/search?q=" + input2 + "&api_key=dc6zaTOxFJmzC",
      
      
       function(response) {
         console.log(response);
-        for(var i = 0;i > 6;i++){
-          $("#gif").append("<img src=" + response.data[i].images.fixed_width.url + ">");  
-        }
-        //$("#gif").append("<img src=" + response.data[19].images.fixed_width.url + ">");
- 
-        
+          $("#gif").append("<img src=" + response.data[1].images.fixed_width_downsampled.url + ">");
+           $("#gif").append("<img src=" + response.data[2].images.fixed_width_downsampled.url + ">");  
+            $("#gif").append("<img src=" + response.data[3].images.fixed_width_downsampled.url + ">");  
+             $("#gif").append("<img src=" + response.data[4].images.fixed_width_downsampled.url + ">");  
+              $("#gif").append("<img src=" + response.data[5].images.fixed_width_downsampled.url + ">");  
       });
   });
-
+});
